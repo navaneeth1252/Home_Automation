6c513eda1b0e92ad5f20caf01f8cd9cc158c6902
navaneeth1252 <51719074+navaneeth1252@users.noreply.github.com>
2019-11-29 14:09:24 +0530

    Create GoogleAssistant1.ino
    Code for connecting to google assistant

---
+++ GoogleAssistant1/GoogleAssistant1.ino
@@ -0,0 +1,105 @@
+//Home Automation using Google Assistant
+#include <ESP8266WiFi.h>
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+ 
+#define Relay1            D1
+#define Relay2            D2
+
+#define WLAN_SSID       "xxxxxxxxxx"             // Your SSID
+#define WLAN_PASS       "xxxxxxxxxxxxxxxxx"        // Your password
+
+/************************* Adafruit.io Setup *********************************/
+
+#define AIO_SERVER      "io.adafruit.com" //Adafruit Server
+#define AIO_SERVERPORT  1883                   
+#define AIO_USERNAME    "xxxxxxxxxxxxx"            // Username of Adafruit IO
+#define AIO_KEY         "xxxxxxxxxxxxxxxxxxxxxxxxxxxx"   // Auth Key of Adafruit IO server
+
+//WIFI CLIENT
+WiFiClient client;
+
+Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
+
+Adafruit_MQTT_Subscribe Light = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME"/feeds/Relay1"); // Feeds name should be same everywhere
+Adafruit_MQTT_Subscribe Fan = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/Relay2");
+
+void MQTT_connect();
+
+void setup() {
+  Serial.begin(115200);
+
+  pinMode(Relay1, OUTPUT);
+  pinMode(Relay2, OUTPUT);
+  
+  // Connect to WiFi access point.
+  Serial.println(); Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(WLAN_SSID);
+
+  WiFi.begin(WLAN_SSID, WLAN_PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+
+  Serial.println("WiFi connected");
+  Serial.println("IP address: "); 
+  Serial.println(WiFi.localIP());
+ 
+  mqtt.subscribe(&Light1);
+  mqtt.subscribe(&Light3);
+  mqtt.subscribe(&Light2);
+  mqtt.subscribe(&Light4);
+}
+
+void loop() {
+ 
+  MQTT_connect();
+  
+
+  Adafruit_MQTT_Subscribe *subscription;
+  while ((subscription = mqtt.readSubscription(20000))) {
+    if (subscription == &Light1) {
+      Serial.print(F("Got: "));
+      Serial.println((char *)Light1.lastread);
+      int Light1_State = atoi((char *)Light1.lastread);
+      digitalWrite(Relay1, Light1_State);
+      
+    }
+    if (subscription == &Light2) {
+      Serial.print(F("Got: "));
+      Serial.println((char *)Light2.lastread);
+      int Light2_State = atoi((char *)Light2.lastread);
+      digitalWrite(Relay2, Light2_State);
+    }
+  }
+
+  
+}
+
+void MQTT_connect() {
+  int8_t ret;
+
+  if (mqtt.connected()) {
+    return;
+  }
+
+  Serial.print("Connecting to MQTT... ");
+
+  uint8_t retries = 5;
+  
+  while ((ret = mqtt.connect()) != 0) {
+    Serial.println(mqtt.connectErrorString(ret));
+    Serial.println("Retrying connection to server");
+    mqtt.disconnect();
+    delay(3000); 
+    retries--;
+    if (retries == 0) {
+      break;
+    }
+  }
+  Serial.println("Connected to server");
+}
+
